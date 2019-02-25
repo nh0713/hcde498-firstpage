078903ecaa20d4c65e6aa53d532a3c8d3b41242e
Nathan Han <nathan0713@gmail.com>
2019-02-24 16:31:39 -0800

    test

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ class RowHomeScreenButtons extends Component {
       return (
           <div class="btnContainer">
             <HomeScreenButton type="My Posts" />
-            <HomeScreenButton type="Sell" />
+            <HomeScreenButton type="Sll" />
             <HomeScreenButton type="Bookmarks" />
           </div>
       );
