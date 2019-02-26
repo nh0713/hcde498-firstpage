6b1ee7002f16b5ffeee224004248c4b151ca8f2c
Nathan Han <nathan0713@gmail.com>
2019-02-26 12:02:30 -0800

    layout and color

--- src/App.js
+++ src/App.js
@@ -1,32 +1,101 @@
 import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, FormLabel, Container, Row, Col } from "react-bootstrap";
 import logo from './logo.svg';
 import './App.css';
 import { BADNAME } from 'dns';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
+        <LoginPage />
+        {/* <SearchPage /> */}
+      </div>
+    );
+  }
+}
 
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.email = "";
+    this.password = "";
+  }
+
+  handleSubmit = (event) => {
+    console.log(this.email);
+    console.log(this.password)
+  }
+
+  emailChange = (event) => {
+    // console.log(event.target.value);
+    this.email = event.target.value;
+  }
+
+  passwordChange = (event) => {
+    // console.log(event.target.value);
+    this.password =event.target.value;
+  }
+
+  render() {
+    return (
+      <div>
         <Banner />
-        <div class="container searchAndButtons">
+        <div className="login">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email" size="large">
+              <FormLabel>Email</FormLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                onChange={this.emailChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" size="large">
+              <FormLabel>Password</FormLabel>
+              <FormControl
+                type="password"
+                onChange={this.passwordChange}
+              />
+            </FormGroup>
+            <Button
+              block
+              size="large"
+              type="submit"
+              // onClick={this.handleSubmit}
+            >
+              Login
+            </Button>
+          </form>
+          <div className="mt-4">
+            <a href="#" >Create Account</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ResultsPage extends Component {
+  render() {
+    return (
+      <div>
+        Test div
+      </div>
+    );
+  }
+}
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <div>
+        <Banner />
+        <Container fluid className="searchAndButtons">
           <SearchBar />
           <RowHomeScreenButtons />
-        </div>
+        </Container>
       </div>
     );
   }
@@ -35,10 +104,15 @@ class App extends Component {
 class Banner extends Component {
   render() {
     return (
-      <div class="container-fluid banner">
+      <header fluid className="banner">
         <h1>Buy and Sell Textbooks Locally</h1>
-      </div>
-    )
+        {/* <div class="search-box">
+          <form>
+            <input class="search-txt" type="text" placeholder="Type to search" aria-label="Search" onChange={this.seachInput}></input>  
+          </form>
+        </div>  */}
+      </header>
+    );
   }
 }
 
@@ -52,21 +126,21 @@ class SearchBar extends Component{
     console.log(this.searchText);
   }
 
-  seachInput = (event) => {
+  searchInput = (event) => {
     this.searchText = event.target.value;
   }
 
   render() {
     return (
-      <div class="search-box">
+      <div className="search-box">
         <form>
-          <input class="search-txt" type="text" placeholder="Type to search" aria-label="Search" onChange={this.seachInput}></input>  
+          <input className="search-txt" type="text" placeholder="Type to search" aria-label="Search" onChange={this.searchInput}></input>  
         </form>        
-        <a class="search-btn" href='#' onClick={this.searchClicked}>
-          <i class="fas fa-search fa-2x"></i>
+        <a className="search-btn" href='#' onClick={this.searchClicked}>
+          <i className="fas fa-search fa-2x"></i>
         </a>
       </div>
-    )
+    );
     
   }
 }
@@ -75,18 +149,26 @@ class HomeScreenButton extends Component {
   render() {
     let type = this.props.type;
 
-    return <a href="#" class="btn m-5 btn-lg" role="button">{type}</a>
+    return <a href="#" className="homeBtn m-5 btn-lg" role="button">{type}</a>;
   }
 }
 
 class RowHomeScreenButtons extends Component {
     render() {
       return (
-          <div class="row justify-content-center">
+        <div className="mt-5">
+          <Row className="homePageBtnRow justify-content-center">
+          <Col>
             <HomeScreenButton type="My Posts" />
+          </Col>
+          <Col>
             <HomeScreenButton type="Sell" />
+          </Col>
+          <Col>
             <HomeScreenButton type="Bookmarks" />
-          </div>
+          </Col>
+          </Row>
+        </div>
       );
     }
 }
