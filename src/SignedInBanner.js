13530d9061f1aef67255f3c6aaec6343d90fc480
Nathan Han <nathan0713@gmail.com>
2019-03-11 16:53:10 -0700

    link fixes

--- src/SignedInBanner.js
+++ src/SignedInBanner.js
@@ -21,8 +21,8 @@ class SignedInBanner extends Component {
           <Container fluid>
                 <Row className="banner">
                     <Col sm={12} md={8} lg={9}>
-                    {/* <Link to='/hcde498-firstpage/'> */}
-                        <Link to='/'>
+                    <Link to='/hcde498-firstpage/'>
+                        {/* <Link to='/'> */}
                             <img src={logo} alt="logo"></img>
                         </Link>
                     </Col>
